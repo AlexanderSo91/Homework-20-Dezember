61b4b74861bd44b41514aacca2de59a76e166e07
zotin <zotinalexander@gmail.com>
2022-12-22 14:46:14 +0100

    Homework done

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -2,6 +2,18 @@ package homework;
 
 public class Main {
     public static void main(String[] args) {
+        Author aleksandrPushkin = new Author("Александр", "Пушкин");
+        Author levTolstoy = new Author("Лев","Толстой");
+        Book evgeniyOnegin = new Book("Евгений Онегин", aleksandrPushkin, 1867);
+        Book warAndPeace = new Book("Война и Мир", levTolstoy, 1867);
 
+        evgeniyOnegin.setYear(1832);
+
+        System.out.println(evgeniyOnegin.getTitle() + " " + evgeniyOnegin.getAuthor().getFirstName()
+                + " " + evgeniyOnegin.getAuthor().getLastNAme() + " " + evgeniyOnegin.getYear());
+
+        System.out.println(aleksandrPushkin);
+        System.out.println(warAndPeace);
     }
 }
+
